ed06bacbd87db8f591a669cb4ebf8b9151fe8767
Aleksandr.rh <sascha.sadov@yandex.ru>
2021-09-01 16:56:24 +0300

    update global declaration

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,7 @@
 declare const IS_DEV: string;
+declare const IS_PROD: string;
 declare const IS_CLIENT: string;
+declare const IS_SERVER: string;
 
 interface Window {
 	__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
